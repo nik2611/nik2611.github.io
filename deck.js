38e6cbb59b12f20a4d4d809acac6b012a6e1ef39
nikhilgangeri <nikhilgangeri@gmail.com>
2022-07-21 18:24:22 +0530

    created deck class and required arrays

--- deck.js
+++ deck.js
@@ -1,5 +1,18 @@
+module.exports = deck;
+
+const cardSuits = ['♤', '♥', '♦', '♧'];
+const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const suitValues = {'J': 11, 'Q': 12, 'K': 13, 'A': 14};
+
+class deck {
+  constructor(suits, values) { 
+     
+  }
+}
+
+
 function deck(a, b) {
     return a + b;
   }
   
-  module.exports = deck;
+  
