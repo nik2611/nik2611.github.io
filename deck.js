9a0b786976553565ccb2f35e1edcedd422207494
nikhilgangeri <nikhilgangeri@gmail.com>
2022-07-21 19:30:53 +0530

    added deck class that generates cards but not working

--- deck.js
+++ deck.js
@@ -1,18 +1,30 @@
-module.exports = deck;
+//module.exports = cards.cards;
 
 const cardSuits = ['♤', '♥', '♦', '♧'];
-const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const cardNumbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const suitValues = {'J': 11, 'Q': 12, 'K': 13, 'A': 14};
 
 class deck {
-  constructor(suits, values) { 
-     
+  constructor(numbers, suits, suitVal) { 
+    this.numbers = numbers;
+    this.suits = suits;
+    this.suitVal = suitVal;
+  }
+
+  cards() {
+     const result = [];
+     for(let i=0; i<this.numbers.length; i++){
+      for(let j=0; j<this.suits.length; j++){
+        let value = isNaN(this.numbers) ? this.suitVal[this.numbers] : parseInt(this.numbers, 10);
+        result.push({display: this.numbers + this.suits, value});
+      }
+     };
+     return result;
   }
 }
 
+let cards = new deck(cardNumbers, cardSuits, suitValues);
+console.log(cards.cards());
 
-function deck(a, b) {
-    return a + b;
-  }
   
   
